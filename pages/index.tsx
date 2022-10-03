8cf8fe0ae536f0e083d59bec4221318dd0cc22a8
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-10-04 00:09:06 +0100

    Styles for homepage main banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
+
+import EmailForm from '../components/emailForm'
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>Hi</div> 
+    <div className={styles.background}>
+      <div className={styles.nav}>
+        <span className={styles.logo}><Image src="/netflix.svg" alt="Logo" width={133} height={36} /></span>
+        <Link href='/'><a className={styles.signIn}>Sign In</a></Link>
+      </div>
+      <div className={styles.emailContainer}>
+        <div className={styles.mainTitle}>Unlimited films, TV programmes and more.</div>
+        <div className={styles.mainSubtitle}>Watch anywhere. Cancel at any time.</div>
+        <EmailForm />
+      </div>
+    </div>
   )
 }
 
